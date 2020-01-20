9ebbcf5909b514a7d7bf7140c957ca5304e41ab6
jaden.liao <jaden.liao@zoom.us>
2020-01-20 14:06:57 +0800

    add router Redirect

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,58 @@
 import React from 'react';
 
 class Welcome extends React.Component{
+
+  componentDidMount() {
+    new Promise(function (resolve, reject) {
+      console.log('start new Promise...');
+      var timeOut = Math.random() * 2;
+      console.log('set timeout to: ' + timeOut + ' seconds.');
+      setTimeout(function () {
+        if (timeOut < 1) {
+          console.log('call resolve()...');
+          resolve('200 OK');
+        }
+        else {
+          console.log('call reject()...');
+          reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+    }).then(function (result) { // 这里的参数result就是resolve方法通过参数传出的内容
+      console.log('Done: ' + result);
+    },function (result) { // 这里的参数result就是reject方法通过参数传出的内容
+      console.log('Failed: ' + result);
+    });
+
+  // 0.5秒后返回input*input的计算结果:
+  function multiply(input) {
+    return new Promise(function (resolve, reject) {
+      console.log('calculating ' + input + ' x ' + input + '...');
+      setTimeout(resolve, 500, input * input);
+    });
+  }
+
+  // 0.5秒后返回input+input的计算结果:
+  function add(input) {
+    return new Promise(function (resolve, reject) {
+      console.log('calculating ' + input + ' + ' + input + '...');
+      setTimeout(resolve, 500, input + input);
+    });
+  }
+
+  var p = new Promise(function (resolve, reject) {
+    console.log('start new Promise...');
+    resolve(123);
+  });
+
+  p.then(multiply)
+    .then(add)
+    .then(multiply)
+    .then(add)
+    .then(function (result) {
+      console.log('Got value: ' + result);
+    });
+  }
+
   render() {
     const todoList = ['hello','hi'];
     const isLogin = true
@@ -9,7 +61,7 @@ class Welcome extends React.Component{
       <ul>
         {
           todoList.map(item=>
-            <li>{item}</li>  
+            <li key={item}>{item}</li>  
           )
         }
       </ul>
